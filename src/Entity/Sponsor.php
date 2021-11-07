67ed55cad0be4c73735c6355d013f11894b512db
maxime <max.poujol21@gmail.com>
2021-11-07 16:21:50 +0100

    v2

--- src/Entity/Sponsor.php
+++ src/Entity/Sponsor.php
@@ -32,7 +32,7 @@ class Sponsor
 
     /**
      * @var string
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private string $url;
 
