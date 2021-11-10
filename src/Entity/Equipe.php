e94e2b7c5b83ba995c58ba48a884c341f2223477
maxime <max.poujol21@gmail.com>
2021-11-10 08:25:20 +0100

    v2 update sponsor

---
+++ src/Entity/Equipe.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EquipeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EquipeRepository::class)
+ */
+class Equipe
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $nomEquipe;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomEquipe(): ?string
+    {
+        return $this->nomEquipe;
+    }
+
+    public function setNomEquipe(string $nomEquipe): self
+    {
+        $this->nomEquipe = $nomEquipe;
+
+        return $this;
+    }
+}
